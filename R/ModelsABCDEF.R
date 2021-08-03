19b3e36c1844bacf26b004acbc6528daca46f26d
Joyce Robbins <jtr13@columbia.edu>
2021-08-03 12:50:30 -0400

    add models example

---
+++ R/ModelsABCDEF.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(GGally)
+library(plotly)
+df <- read_csv("data/example.csv")
+df$group <- c(rep("top 6", 6), rep("other", 37))
+g <- ggparcoord(df, columns = 2:7, scale = "globalminmax",
+                groupColumn = 8, alphaLines = .5) +
+  scale_color_manual(values = c('#d95f02','#1b9e77'),
+                     guide = guide_legend(reverse = TRUE))
+ggplotly(g)
