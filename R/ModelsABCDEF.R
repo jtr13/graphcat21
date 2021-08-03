faff401b4ae35d6a754948f131e3a789e60590c9
Joyce Robbins <jtr13@columbia.edu>
2021-08-03 13:48:23 -0400

    add pdf

--- R/ModelsABCDEF.R
+++ R/ModelsABCDEF.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(GGally)
-library(plotly)
-df <- read_csv("data/example.csv")
+df <- read_csv("../data/example.csv")
 df$group <- c(rep("top 6", 6), rep("other", 37))
 g <- ggparcoord(df, columns = 2:7, scale = "globalminmax",
                 groupColumn = 8, alphaLines = .5) +
   scale_color_manual(values = c('#d95f02','#1b9e77'),
-                     guide = guide_legend(reverse = TRUE))
-ggplotly(g)
+                     guide = guide_legend(reverse = TRUE)) +
+  scale_y_continuous(breaks = seq(0, 100, 10))
+g
