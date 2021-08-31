ac8567a37946c5ed5115bc13b35c56afbd9662e9
Joyce Robbins <jtr13@columbia.edu>
2021-08-31 12:57:35 -0400

    edits

--- labs/PCP_codealong.R
+++ labs/PCP_codealong.R
@@ -1,18 +1,46 @@
 library(tidyverse)
 library(GGally)
 library(plotly)
+
+library(palmerpenguins)
+str(penguins)
+head(penguins)
+
+# scatterplot matrix
+
+colors3 <- c("orange","cyan","blue")
+
+lattice::splom(penguins[,3:6], cex = .5, pch = 16,
+               col = colors3[penguins$species],
+               key = list(space="bottom",
+                          points=list(pch = 16, col=colors3),
+                          text=list(levels(penguins$species))))
+
+
+ggparcoord(penguins)
+
+# numeric columns only
+ggparcoord(penguins, columns = 3:6)
+
+# correlations
+ggparcoord(penguins, columns = 3:6, alphaLines = .3)
+
+# clustering
+ggparcoord(penguins, columns = 3:6, alphaLines = .3, groupColumn = 1)
+
+ggparcoord(penguins, columns = 3:6, alphaLines = .3,
+           groupColumn = 1, splineFactor = 10)
+
+
+
+
+
+
 df <- read_csv("data/example.csv")
 
-tidy_df <- df %>% pivot_longer(-`Variable Name`, names_to = "model", values_to = "percent")
+head(df)
 
-g <- ggplot(tidy_df, aes(x = model, y = percent, group = `Variable Name`)) +
-  geom_line()
 
-g
-
-g + geom_text(data = df[,1:2], aes(x = .8, y = `Model A`, label = `Variable Name`))
-
-plotly::ggplotly(g)
 
 # Back to original data frame
 ggparcoord(df[-1], scale = "globalminmax")
@@ -33,30 +61,18 @@ ggparcoord(df, columns = 2:7, scale="globalminmax",
                      guide = guide_legend(reverse = TRUE)) +
   scale_y_continuous(breaks = seq(0, 100, 10))
 
-library(palmerpenguins)
-str(penguins)
-ggparcoord(penguins)
-ggparcoord(penguins, columns = 3:6)
-ggparcoord(penguins, columns = 3:6, alphaLines = .3)
-ggparcoord(penguins, columns = 3:6, alphaLines = .3, groupColumn = 1)
-ggparcoord(penguins, columns = 3:6, alphaLines = .3, groupColumn = 1,
-           splineFactor = 10)
 
-colors3 <- c("orange","cyan","blue")
 
-lattice::splom(penguins[,3:6], cex = .5, pch = 16,
-               col = colors3[penguins$species],
-               key = list(space="bottom",
-                          points=list(pch = 16, col=colors3),
-                          text=list(levels(penguins$species))))
 
-pca <- prcomp(penguins[complete.cases(penguins),3:6], scale. = TRUE)
-biplot(pca)
 
-ggplot(cbind(penguins_complete, pca$x),
-       aes(PC1, PC2, color = species, fill = species)) +
-  stat_ellipse(geom="polygon", color = "grey20", alpha = .2) +
-  geom_point(shape = 21, color = "black") +
-  ggtitle("PCA plot")
+# build it yourself
+tidy_df <- df %>% pivot_longer(-`Variable Name`, names_to = "model", values_to = "percent")
 
-ggplot(penguins_complete, aes(bill_length_mm, bill_depth_mm, color = species)) + geom_point() + stat_ellipse(geom="polygon", alpha = .2, aes(fill = species)) + ggtitle("")
+g <- ggplot(tidy_df, aes(x = model, y = percent, group = `Variable Name`)) +
+  geom_line()
+
+g
+
+g + geom_text(data = df[,1:2], aes(x = .8, y = `Model A`, label = `Variable Name`))
+
+plotly::ggplotly(g)
